6e6ec972beb196f34ad2056ed706499cd5f7d0f0
heemang2001 <babypooh2001@naver.com>
2021-10-18 16:57:01 +0900

    케빈 베이컨(BFS)

--- DFS+BFS/케빈 베이컨의 6단계 법칙.cpp
+++ DFS+BFS/케빈 베이컨의 6단계 법칙.cpp
@@ -11,28 +11,11 @@ using namespace std;
 int N, M;
 int Answer;
 int Friends[101][101];
-
-bool cmp(pair<int,int> p1, pair<int, int> p2)
-{
-	if (p1.second == p2.second)
-	{
-		return p1.first < p2.first;
-	}
-		
-	return p1.second < p2.second;	
-}
+bool bVisited[101];
 
 int main()
 {
-	cin >> N >> M;	
-
-	fill(&Friends[0][0], &Friends[100][101], INF);
-	vector<pair<int,int>> vecAnswer;
-
-	for (int i = 1; i <= N; i++)
-	{
-		Friends[i][i] = 0;
-	}
+	cin >> N >> M;
 
 	for (int i = 0; i < M; i++)
 	{
@@ -45,40 +28,117 @@ int main()
 		Friends[nTemp2][nTemp1] = 1;
 	}
 
-	// (플로이드 와샬)
-	// 
-	// 거쳐가는 노드
-	for (int j = 1; j <= N; j++)
-	{
-		// 출발
-		for (int i = 1; i <= N; i++)
-		{
-			// 도착
-			for (int k = 1; k <= N; k++)
-			{
-				if (Friends[i][j] + Friends[j][k] < Friends[i][k])
-				{
-					Friends[i][k] = Friends[i][j] + Friends[j][k];
-				}
-			}
-		}
-	}
+	// 현재번호, 현재층
+	queue<pair<int, int>> q;
+
+	int sum = 0;
+	int result = INF;
 
 	for (int i = 1; i <= N; i++)
 	{
-		int nTemp = 0;
+		q.push({ i,0 });
+		bVisited[i] = true;
+		sum = 0;
 
-		for (int j = 1; j <= N; j++)
+		while (!q.empty())
 		{
-			nTemp += Friends[i][j];
+			int cur_Num = q.front().first;
+			int next_Num = q.front().second + 1;
+			q.pop();
+
+			for (int j = 1; j <= N; j++)
+			{
+				if (Friends[cur_Num][j] == 1 && !bVisited[j])
+				{
+					bVisited[j] = true;
+					q.push({ j,next_Num });
+					sum += next_Num;
+				}
+			}
 		}
 
-		vecAnswer.push_back({i,nTemp});
+		if (sum < result)
+		{
+			result = sum;
+			Answer = i;
+		}
+
+		memset(bVisited, false, sizeof(bVisited));
 	}
 
-	sort(vecAnswer.begin(), vecAnswer.end(), cmp);
-
-	cout << vecAnswer.front().first << '\n';
+	cout << Answer << '\n';
 
 	return 0;
 }
+
+
+//bool cmp(pair<int,int> p1, pair<int, int> p2)
+//{
+//	if (p1.second == p2.second)
+//	{
+//		return p1.first < p2.first;
+//	}
+//		
+//	return p1.second < p2.second;	
+//}
+//
+//int main()
+//{
+//	cin >> N >> M;	
+//
+//	fill(&Friends[0][0], &Friends[100][101], INF);
+//	vector<pair<int,int>> vecAnswer;
+//
+//	for (int i = 1; i <= N; i++)
+//	{
+//		Friends[i][i] = 0;
+//	}
+//
+//	for (int i = 0; i < M; i++)
+//	{
+//		int nTemp1;
+//		int nTemp2;
+//
+//		cin >> nTemp1 >> nTemp2;
+//
+//		Friends[nTemp1][nTemp2] = 1;
+//		Friends[nTemp2][nTemp1] = 1;
+//	}
+//
+//	// (플로이드 와샬)
+//	// 
+//	// 거쳐가는 노드
+//	for (int j = 1; j <= N; j++)
+//	{
+//		// 출발
+//		for (int i = 1; i <= N; i++)
+//		{
+//			// 도착
+//			for (int k = 1; k <= N; k++)
+//			{
+//				if (Friends[i][j] + Friends[j][k] < Friends[i][k])
+//				{
+//					Friends[i][k] = Friends[i][j] + Friends[j][k];
+//				}
+//			}
+//		}
+//	}
+//
+//	for (int i = 1; i <= N; i++)
+//	{
+//		int nTemp = 0;
+//
+//		for (int j = 1; j <= N; j++)
+//		{
+//			nTemp += Friends[i][j];
+//		}
+//
+//		vecAnswer.push_back({i,nTemp});
+//	}
+//
+//	sort(vecAnswer.begin(), vecAnswer.end(), cmp);
+//
+//	cout << vecAnswer.front().first << '\n';
+//
+//	return 0;
+//}
